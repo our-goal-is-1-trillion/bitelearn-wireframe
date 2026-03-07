7df13594a20d8e2c5ff4b2e9705b56583e2c9d2c
johneybi <eun082971@gmail.com>
2026-03-07 09:55:02 +0900

    전체 리팩토링

--- src/components/features/onboarding/OnboardingModal.tsx
+++ src/components/features/onboarding/OnboardingModal.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Button } from '@/components/ui/button';
-import { ONBOARDING_DATA } from '@/constants/onboardingData';
+import { ONBOARDING_DATA } from './onboardingData'
 
 type OnboardingModalProps = {
   isOpen: boolean;
